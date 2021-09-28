f1a48ffc86857f3a194a8f62d58da885794a374b
dwc04112 <dwc04112@gmail.com>
2021-09-28 18:38:52 +0900

    RequestMapping -> GetMapping

--- src/main/java/kr/ac/daegu/springbootapi/model/TestDTO.java
+++ src/main/java/kr/ac/daegu/springbootapi/model/TestDTO.java
@@ -1,2 +1,14 @@
-package kr.ac.daegu.springbootapi.model;public class TestDTO {
+package kr.ac.daegu.springbootapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor //testDTO가 가지고있는 모든 Constructor 만들어라 (lombok)
+@Getter
+@Setter
+
+public class TestDTO {
+    private int num;
+    private String name;
 }
