d5eea74483faed5394e6574ccc75cb90ca342b0d
dwc04112 <dwc04112@gmail.com>
2021-09-29 16:28:21 +0900

    boardMapper와 TestMapper 분리

--- src/main/java/kr/ac/daegu/springbootapi/board/model/BoardDTO.java
+++ src/main/java/kr/ac/daegu/springbootapi/board/model/BoardDTO.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 import java.sql.Time;
 import java.util.Date;
 
-@AllArgsConstructor //testDTO가 가지고있는 모든 Constructor 만들어라 (lombok)
+@AllArgsConstructor
 @Getter
 @Setter
 public class BoardDTO {
@@ -16,8 +16,8 @@ public class BoardDTO {
     private String author;               // 작성자 이름
     private String subject;              // 글 제목
     private String content;              // 글 컨텐츠
-    private java.sql.Date writeDate;              // 작성 날짜
-    private java.sql.Timestamp writeTime;              // 작성 시간
+    private Date writeDate;              // 작성 날짜
+    private Time writeTime;              // 작성 시간
     private int readCount;               // 조회수
     private int commentCount;            // 댓글 갯수
     private String password;             // 수정 삭제를 위한 패스워드
